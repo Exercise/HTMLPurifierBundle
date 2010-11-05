e9db3d4da167f21bb8063e23c88cb278b6067fdf
jseverson <j.severson@yahoo.com>
2010-11-05 08:26:12 -0700

    adding more options that html purifier allows

--- HTMLPurifier.php
+++ HTMLPurifier.php
@@ -19,11 +19,22 @@ class HTMLPurifier
         }
 
         if(!$this->config) {
-            $config = \HTMLPurifier_Config::createDefault();
-            $config->set('Cache.SerializerPath', $this->options['cache_dir']);
-//                $config->set('Filter.YouTube', true);
+            $this->config = \HTMLPurifier_Config::createDefault();
+            $this->config->set('Cache.SerializerPath', $this->options['cache_dir']);
+            if(isset($this->options['enable_youtube']) && $this->options['enable_youtube']) {
+                $this->config->set('Filter.YouTube', true);
+            }
+            if(isset($this->options['allowed_html']) && $this->options['allowed_html'] !== null) {
+                $this->config->set('HTML.Allowed', $this->options['allowed_html']);
+            }
+            if(isset($this->options['base_uri']) && $this->options['base_uri'] !== null) {
+                $this->config->set('URI.Base', $this->options['base_uri']);
+            }
+            if(isset($this->options['absolute_uri']) && $this->options['absolute_uri'] !== null) {
+                $this->config->set('URI.MakeAbsolute', $this->options['absolute_uri']);
+            }
         }
-        $purifier = new \HTMLPurifier($config);
+        $purifier = new \HTMLPurifier($this->config);
 
         return $purifier->purify($data);
     }
