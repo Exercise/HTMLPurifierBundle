3dff91a045c58457584dbf3b9ba938b20d0017cb
ornicar <thibault.duplessis@gmail.com>
2011-02-01 12:27:01 -0800

    Implement missing methods in bundle class

--- HTMLPurifierBundle.php
+++ HTMLPurifierBundle.php
@@ -6,4 +6,13 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class HTMLPurifierBundle extends Bundle
 {
+    public function getNamespace()
+    {
+        return __NAMESPACE__;
+    }
+
+    public function getPath()
+    {
+        return strtr(__DIR__, '\\', '/');
+    }
 }
