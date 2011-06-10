2d2444949d630d9e405baeeb986e99ccaf92f169
ornicar <thibault.duplessis@gmail.com>
2011-06-10 10:30:41 -0700

    Revert "Remove weird bundle boot method"
    This reverts commit 781de6348e791dbb5ed4d604fed3c7d45753d08c.

--- ExerciseHTMLPurifierBundle.php
+++ ExerciseHTMLPurifierBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ExerciseHTMLPurifierBundle extends Bundle
 {
+    public function boot()
+    {
+        new \HTMLPurifier_Bootstrap();
+    }
 }
