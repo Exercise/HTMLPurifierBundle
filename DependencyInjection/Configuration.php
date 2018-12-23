c1061f32aaa4dd5868bc1a35b81676ae5ec9c433
Mathieu Piot <math.piot@gmail.com>
2018-12-23 16:19:47 +0100

    Fix Tree root deprecation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,8 +12,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('exercise_html_purifier');
+        $treeBuilder = new TreeBuilder('exercise_html_purifier');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('exercise_html_purifier');
+        }
 
         $rootNode
             ->useAttributeAsKey('name')
